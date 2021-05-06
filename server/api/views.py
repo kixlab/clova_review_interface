3aadbd4ad23b77ced69cc54576e61fc990f98bc3
eunyoungko <eunyoung4444@gmail.com>
2021-05-07 01:36:53 +0900

    label author info

--- server/api/views.py
+++ server/api/views.py
@@ -11,7 +11,7 @@ import json
 def checkUser(request):
     if request.method == 'GET':
         mturk_id = request.GET['mturk_id']
-        print(mturk_id)
+        print("Hi",mturk_id)
         user, created = User.objects.get_or_create(mturk_id=mturk_id)
         task_done = len(list(User.objects.filter(instrEnded = True))) > 15
         response = {
