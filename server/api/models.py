d9ff9efcbbfdb7ed69e139174b1a991cf8176cd2
eunyoungko <eunyoung4444@gmail.com>
2021-05-11 23:28:29 +0900

    jsonize respoinse

--- server/api/models.py
+++ server/api/models.py
@@ -114,7 +114,7 @@ class UserSubcat(models.Model):
 
 class Annotation(models.Model):
     user=models.ForeignKey('User', on_delete=models.CASCADE)
-    document=models.ForeignKey('Document', on_delete=models.SET_NULL)
+    document=models.ForeignKey('Document', on_delete=models.SET_NULL, null=True)
     group_id=models.IntegerField(default=1)
     box_id=models.IntegerField(default=1)
     status=models.BooleanField(default=False)
