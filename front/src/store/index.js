d010c37976ac11d5ceee0607f417457f639cdee0
eunyoungko <eunyoung4444@gmail.com>
2021-05-11 20:47:28 +0900

    update devserver ip address

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
   })],
   state: {
     mturk_id: null,
-    server_url: 'http://13.209.4.171:8000',
+    server_url: 'http://3.34.46.125:8000',
     image_order: 0
   },
   mutations: {
