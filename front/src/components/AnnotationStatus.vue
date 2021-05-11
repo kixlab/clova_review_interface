779866fe435567028fd78865fe76a5c9d41562be
eunyoungko <eunyoung4444@gmail.com>
2021-05-12 03:35:46 +0900

    reserve recent annotations only

--- front/src/components/AnnotationStatus.vue
+++ front/src/components/AnnotationStatus.vue
@@ -90,8 +90,6 @@ export default {
       this.$store.subscribeAction({after: (action) => {
         if (action.type === 'updateImageBoxes') {
           this.image_box = this.$store.getters.getImageBoxes;
-          console.log("Hi");
-          this.reset();
         }
         if (action.type === 'updateAnnotatedBoxes') {
           this.annotated_box = this.$store.getters.getAnnotatedBoxes;
