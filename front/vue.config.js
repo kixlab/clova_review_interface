3f341232a42bd72be6cb7d2d156ec8f999abedda
eunyoungko <eunyoung4444@gmail.com>
2021-05-16 15:09:40 +0900

    socket hostcheck disabled

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     devServer:{
+        disableHostCheck:true,
         host: '3.34.46.125',
         port: 8080,
-        public: 'localhost:8080',
+        public: '0.0.0.0:8080',
     },
+    pablicPath: "/"
 };
