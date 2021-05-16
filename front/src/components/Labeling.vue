fe5dabb594026fd19656a695e6154a15aac01f05
eunyoungko <eunyoung4444@gmail.com>
2021-05-16 23:23:03 +0900

    reset filtered list when annotate

--- front/src/components/Labeling.vue
+++ front/src/components/Labeling.vue
@@ -303,7 +303,7 @@ export default {
             var ids=agroup.boxes_id.replace("[","").replace("]","").replace(" ","").replace(', ',',').split(',')
             for(var id in ids){
               var box_id=parseInt(ids[id])
-              var currBox=currImageBox[box_id]
+              var currBox=currImageBox.find(item => {return item.id==box_id})
               currBox.annotated=true
               group.push(currBox)
             }
