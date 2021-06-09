f2b12c8652bd0d69763140d7ca306b3b39fc3a8d
eunyoungko <eunyoung4444@gmail.com>
2021-06-10 05:53:08 +0900

    deferred annotation introduced

--- front/src/components/SubmitButton.vue
+++ front/src/components/SubmitButton.vue
@@ -26,7 +26,7 @@ export default {
   name: "SubmitButton",
   methods: {
     onSubmit: function() {
-      var doctype=this.$router.currentRoute.fullPath.split('/')[-1];
+      var doctype=this.$router.currentRoute.fullPath.split('/');
       console.log(doctype);
      // this.$router.push('../annot-done')
  }
