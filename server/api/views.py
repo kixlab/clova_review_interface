5d4299c6781556e7cdc51cfc2e046a8d398338c6
eunyoungko <eunyoung4444@gmail.com>
2021-06-29 11:02:25 +0200

    user foreignkey redefined

--- server/api/views.py
+++ server/api/views.py
@@ -19,6 +19,7 @@ from django.db.models import Max
 @permission_classes([AllowAny])
 def signup(request):
     username = request.POST.get('username')
+    print(request.POST)
     print('Hi',username)
     password = username
     if len(User.objects.filter(username=username))==0:
