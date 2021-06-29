1dcccf8594f9141a7872f54189f83e28f0d036c1
eunyoungko <eunyoung4444@gmail.com>
2021-06-29 11:14:07 +0200

    revise user profile receiver

--- server/api/models.py
+++ server/api/models.py
@@ -40,9 +40,12 @@ class Profile(models.Model):
         return self.user.username + self.group
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, **kwargs):
-    print(instance)
-    Profile.objects.create(user=instance, doctype='receipt').save()
+def create_user_profile(sender, instance,created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, doctype='receipt')
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 """ # Create your models here.
 class User(models.Model):
