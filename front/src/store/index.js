4659a7b7f54ac28481aafa854a8a19bbe8d521c1
eunyoungko <eunyoung4444@gmail.com>
2021-07-01 02:51:55 +0200

    dynamic status update

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -43,17 +43,17 @@ export default new Vuex.Store({
   getters: {
     image_url: state => {
       var docType= router.currentRoute.params.docType
-      var image_order=state.image_order
+      var image_order=state.image_order+state.start_image_no;
       
       var three_digit_id = ("00" + image_order).slice(-3);
-      console.log(state.start_image_no);
+//      console.log(state.start_image_no);
       console.log("server_url ** ", state.server_url + '/media/'+docType+'/'+docType+'_00' + three_digit_id + '.png')
       return state.server_url + '/media/'+docType+'/'+docType+'_00' + three_digit_id + '.png'
       
     },
     json_url: state => {
       var docType= router.currentRoute.params.docType
-      var image_order=state.image_order
+      var image_order=state.image_order+state.start_image_no;
 
       var three_digit_id = ("00" + image_order).slice(-3);
       console.log("json_url **", state.server_url + '/media/'+docType+'/'+docType+'_00' + three_digit_id + '.json')
