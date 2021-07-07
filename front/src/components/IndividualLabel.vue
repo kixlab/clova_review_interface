d8bd4e96a4569144ff34a696b5a56366c5763570
eunyoungko <eunyoung4444@gmail.com>
2021-07-07 18:15:41 +0200

    extend prog bar

--- front/src/components/IndividualLabel.vue
+++ front/src/components/IndividualLabel.vue
@@ -32,6 +32,16 @@
                             </div>
                         </div>
                     </v-col>
+                    <v-col cols="3" v-for="(userannot, index) in worker_annots" :key="index" style="border-right: 1px solid black;">
+                        <!--{{image_box.map(v => [v.box_id, v.text])}}-->
+                        <div v-for="box in userannot.annotations" :key="box.box_id">
+                            {{user}}
+                            <v-btn x-small outlined v-bind:class="{success: box.confidence, error: (box.subcat=='N/A'), warning: !box.confidence}"> 
+                                    {{box.cat}}-{{box.subcat}} 
+                            </v-btn>
+                        </div>
+                        <!--{{this.$store.getters.getAnnotatedBoxes}}-->
+                    </v-col>
                     <v-col cols="3" v-for="(user, index) in worker_annots" :key="index" style="border-right: 1px solid black;">
                         <!--{{image_box.map(v => [v.box_id, v.text])}}-->
                         <div v-for="box in worker_annots[index]" :key="box.box_id">
