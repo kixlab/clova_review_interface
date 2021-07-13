2e36e5807015fdddb15b3669d96509b53e4469ff
eunyoungko <eunyoung4444@gmail.com>
2021-07-13 21:51:44 +0200

    get worker list and annotations by worker

--- front/src/components/OverallLabel.vue
+++ front/src/components/OverallLabel.vue
@@ -24,12 +24,34 @@ export default {
     },
     data() {
         return {
-
+            worker: 'A8F6JFG0WSELT', // 지금 살펴보고자 하는 worker --> 나중에 worker정보 보여주는 view의 state로 가져가도 될듯!
+            worker_list: [],
+            worker_detail: {"username": null, "annotations":[], "start_time": null, "end_time":null},
+            every_annotations: []
         };
     },
 
     mounted() {
-        //const self = this;
+        const self = this;
+
+        setTimeout( function(){
+        axios.get(self.$store.state.server_url+'/api/get-workers',{
+        params:{
+            doctype: self.$route.params.docType
+        }
+        }).then(function(res){
+            self.worker_list = res.data;
+            console.log(res.data);
+        })},500);
+
+        setTimeout( function(){
+        axios.get(self.$store.state.server_url+'/api/get-annotations-by-worker',{
+        params:{
+            mturk_id: self.worker
+        }
+        }).then(function(res){
+            console.log(res.data);
+        })},500);
     },
 
     methods: {
