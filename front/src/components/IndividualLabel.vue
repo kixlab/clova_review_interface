077b3fef36b9ffdd6b0044a90d40bf97401dac15
eunyoungko <eunyoung4444@gmail.com>
2021-07-13 17:24:26 +0200

    make api call for mturk server

--- front/src/components/IndividualLabel.vue
+++ front/src/components/IndividualLabel.vue
@@ -146,7 +146,7 @@ export default {
         }})
 
         setTimeout( function(){
-        axios.get(self.$store.state.server_url+'/api/get-worker-annotations/',{
+        axios.get(self.$store.state.server_url+'/api/get-every-annotations/',{
         params:{
             doctype: self.$route.params.docType,
             image_id: self.$store.state.image_order + self.$store.state.start_image_no
