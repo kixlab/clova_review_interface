e7681254eb513694b17ac8d1bcf2b5be6c54b935
eunyoungko <eunyoung4444@gmail.com>
2021-08-24 05:59:10 +0900

    get close to suggestions

--- front/src/components/CloseToResolution.vue
+++ front/src/components/CloseToResolution.vue
@@ -234,7 +234,7 @@ export default {
             this.sel_cat = cat.cat
             this.sel_subcat = cat.subcat
 
-            this.suggestions_show = this.suggestions_all.filter(v => v.cat === this.sel_cat).subcat.filter(v => v.subcat === this.sel_subcat).suggestions;
+            this.suggestions_show = this.suggestions_all.filter(v => v.cat === this.sel_cat)[0].subcat.filter(v => v.subcat === this.sel_subcat)[0].suggestions;
             
             // 새로운 label 누를 때 다 초기화 시키기 위해..
             this.selectedBoxes = []
