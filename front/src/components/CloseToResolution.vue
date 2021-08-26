bd2ea56a3f708bb5b17a332b65f95b68970dd5bb
jeongeonp <jeongeonpark1@gmail.com>
2021-08-26 16:02:23 +0900

    Fix close-to error

--- front/src/components/CloseToResolution.vue
+++ front/src/components/CloseToResolution.vue
@@ -407,7 +407,7 @@ export default {
 
             axios.post(self.$store.state.server_url + '/dashboard/save-close-to-'+dest+'/', {
                 mturk_id: self.$store.state.mturk_id, 
-                annotation_pks:self.selectedBoxes_full.map(v => v.annotation_pk),
+                annotation_pks:self.selectedBoxes.map(v => v.annotation_pk),
                 category:self.cat,
                 subcategory:self.subcat,
                 description: '',//self.description,
