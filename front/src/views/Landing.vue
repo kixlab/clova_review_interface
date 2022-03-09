07d76a935f49dde754ae85d080824f91a99d4fda
jeongeonp <jeongeonpark1@gmail.com>
2022-03-09 21:12:31 +0900

    New branch initial commit

--- front/src/views/Landing.vue
+++ front/src/views/Landing.vue
@@ -42,20 +42,6 @@
       Begin (event flyer) <v-icon>mdi-arrow-right</v-icon>
     </v-btn>
   </v-row>
-  <v-row justify="center" style="margin-top: 20px">
-    <v-btn @click="seeAnnotations('receipt', 'baseline')" color="indigo lighten-3" class="mr-4">
-      receipt, baseline
-    </v-btn>
-    <v-btn @click="seeAnnotations('receipt', 'proposed')" color="indigo lighten-3" class="mr-4">
-      receipt, proposed
-    </v-btn>
-    <v-btn @click="seeAnnotations('event', 'baseline')" color="indigo lighten-3" class="mr-4">
-      event, baseline
-    </v-btn>
-    <v-btn @click="seeAnnotations('event','proposed')" color="indigo lighten-3" class="mr-4">
-      event, proposed
-    </v-btn>
-  </v-row>
 </v-container>
 </template>
 
